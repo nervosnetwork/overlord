61dbbacbec006c449862a186490b66a0a6afd2cc
Eason <kaoimin@qq.com>
2019-08-21 15:19:27 +0800

    impl partial eq for consensus error

--- src/error.rs
+++ src/error.rs
@@ -1,8 +1,10 @@
 #![allow(dead_code)]
+use std::cmp::{Eq, PartialEq};
+
 use derive_more::Display;
 
 /// Overlord consensus error.
-#[derive(Debug, Display, PartialEq, Eq)]
+#[derive(Debug, Display)]
 pub enum ConsensusError {
     ///
     #[display(fmt = "Invalid address")]
@@ -28,7 +30,31 @@ pub enum ConsensusError {
     ///
     #[display(fmt = "Self round is {}, vote round is {}", local, vote)]
     RoundDiff { local: u64, vote: u64 },
+    ///
+    #[display(fmt = "Self check not pass {}", _0)]
+    SelfCheckErr(String),
     /// Other error.
     #[display(fmt = "Other error {}", _0)]
     Other(String),
 }
+
+impl PartialEq for ConsensusError {
+    fn eq(&self, other: &Self) -> bool {
+        use self::ConsensusError::*;
+        match (self, other) {
+            (InvalidAddress, InvalidAddress)
+            | (TriggerSMRErr(_), TriggerSMRErr(_))
+            | (MonitorEventErr(_), MonitorEventErr(_))
+            | (ThrowEventErr(_), ThrowEventErr(_))
+            | (ProposalErr(_), ProposalErr(_))
+            | (PrevoteErr(_), PrevoteErr(_))
+            | (PrecommitErr(_), PrecommitErr(_))
+            | (SelfCheckErr(_), SelfCheckErr(_)) => true,
+            (RoundDiff { local: m, vote: n }, RoundDiff { local: p, vote: q }) => m == p && n == q,
+            (Other(x), Other(y)) => x == y,
+            _ => false,
+        }
+    }
+}
+
+impl Eq for ConsensusError {}
