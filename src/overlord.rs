e804b31e6dc6a1bf3677232bfe56bedf4302ed2e
Eason <kaoimin@qq.com>
2020-02-09 21:51:02 +0800

    add drop log

--- src/overlord.rs
+++ src/overlord.rs
@@ -1,3 +1,4 @@
+use std::ops::Drop;
 use std::sync::Arc;
 
 use creep::Context;
@@ -107,6 +108,12 @@ where
 /// An overlord handler to send messages to an overlord instance.
 #[derive(Clone, Debug)]
 pub struct OverlordHandler<T: Codec>(UnboundedSender<(Context, OverlordMsg<T>)>);
+
+impl<T: Codec> Drop for OverlordHandler<T> {
+    fn drop(&mut self) {
+        log::error!("Overlord: OverlordHandler dropping!");
+    }
+}
 
 impl<T: Codec> OverlordHandler<T> {
     fn new(tx: UnboundedSender<(Context, OverlordMsg<T>)>) -> Self {
