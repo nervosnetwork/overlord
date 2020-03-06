7a274cafada8368a1d401ff2f4e22ec88c45b1dd
rev-chaos <wancencen@cryptape.com>
2020-03-06 11:46:42 +0800

    refactor: change AuthorityManage and remove old height (#84)
    * change AuthorityManage, remove old height
    
    * format Status::new

--- src/utils/auth_manage.rs
+++ src/utils/auth_manage.rs
@@ -9,143 +9,13 @@ use crate::types::{Address, Node};
 use crate::utils::rand_proposer::get_random_proposer_index;
 use crate::ConsensusResult;
 
-/// Authority manage consits of the current height authority manage and the last height authority
-/// manage The last height authority manage which is optional is used to check the correctness of
-/// last height's message. The last height's authority manage should be `Some` unless current height
-/// is `0` or `1`.
-#[display(fmt = "Authority List {:?}", current)]
-#[derive(Clone, Debug, Display, PartialEq, Eq)]
-pub struct AuthorityManage {
-    current: HeightAuthorityManage,
-    last:    Option<HeightAuthorityManage>,
-}
-
-impl AuthorityManage {
-    /// Create a new authority management.
-    pub fn new() -> Self {
-        AuthorityManage {
-            current: HeightAuthorityManage::new(),
-            last:    None,
-        }
-    }
-
-    /// Update a new height of authority list. If the argument `reserve_old` is `true`, the old
-    /// authority will be reserved, otherwise, it will be cleared.
-    pub fn update(&mut self, authority_list: &mut Vec<Node>, reserve_old: bool) {
-        self.last = if reserve_old {
-            Some(self.current.clone())
-        } else {
-            None
-        };
-        self.current.update(authority_list);
-    }
-
-    /// Set the last height's authority list when the node leap to a higher height. In this
-    /// situation, an authority list of `current_height - 1` is required to guarantee the
-    /// consensus liveness.
-    pub fn set_last_list(&mut self, authority_list: &mut Vec<Node>) {
-        let mut auth_manage = HeightAuthorityManage::new();
-        auth_manage.update(authority_list);
-        self.last = Some(auth_manage);
-    }
-
-    /// Get a vote weight that correspond to the given address. Return `Err` when the given address
-    /// is not in the authority list.
-    pub fn get_vote_weight(&self, addr: &Address) -> ConsensusResult<&u32> {
-        self.current.get_vote_weight(addr)
-    }
-
-    /// Get a proposer address of the height by a given seed. Return `Err` when `is_current` is
-    /// `false`, and the when last height's authority management is `None`.
-    pub fn get_proposer(
-        &self,
-        height: u64,
-        round: u64,
-        is_current: bool,
-    ) -> ConsensusResult<Address> {
-        if is_current {
-            self.current.get_proposer(height, round)
-        } else if let Some(auth_list) = self.last.clone() {
-            auth_list.get_proposer(height, round)
-        } else {
-            Err(ConsensusError::Other(
-                "There is no authority list cache of last height".to_string(),
-            ))
-        }
-    }
-
-    /// Calculate whether the sum of vote weights from bitmap is above 2/3. Return `Err` when
-    /// `is_current` is `false`, and the when last height's authority management is `None`.
-    pub fn is_above_threshold(&self, bitmap: &[u8], is_current: bool) -> ConsensusResult<bool> {
-        if is_current {
-            self.current.is_above_threshold(bitmap)
-        } else if let Some(auth_list) = self.last.clone() {
-            auth_list.is_above_threshold(bitmap)
-        } else {
-            Err(ConsensusError::Other(
-                "There is no authority list cache of last height".to_string(),
-            ))
-        }
-    }
-
-    /// According the given bitmap to get the voters address to verify aggregated vote.
-    pub fn get_voters(&self, bitmap: &[u8], is_current: bool) -> ConsensusResult<Vec<Address>> {
-        if is_current {
-            self.current.get_voters(bitmap)
-        } else if let Some(auth_list) = self.last.clone() {
-            auth_list.get_voters(bitmap)
-        } else {
-            Err(ConsensusError::Other(
-                "There is no authority list cache of last height".to_string(),
-            ))
-        }
-    }
-
-    /// Check whether the authority management contains the given address. Return `Err` when
-    /// `is_current` is `false`, and the last height's authority management is `None`.
-    pub fn contains(&self, address: &Address, is_current: bool) -> ConsensusResult<bool> {
-        if is_current {
-            Ok(self.current.contains(address))
-        } else if let Some(auth_list) = self.last.clone() {
-            Ok(auth_list.contains(address))
-        } else {
-            Err(ConsensusError::Other(
-                "There is no authority list cache of last height".to_string(),
-            ))
-        }
-    }
-
-    /// Get the sum of the vote weights. Return `Err` when `is_current` is `false`, and the last
-    /// height's authority management is `None`.
-    pub fn get_vote_weight_sum(&self, is_current: bool) -> ConsensusResult<u64> {
-        if is_current {
-            Ok(self.current.get_vote_weight_sum())
-        } else if let Some(auth_list) = self.last.clone() {
-            Ok(auth_list.get_vote_weight_sum())
-        } else {
-            Err(ConsensusError::Other(
-                "There is no authority list cache of last height".to_string(),
-            ))
-        }
-    }
-
-    /// Get the length of the current authority list.
-    pub fn current_len(&self) -> usize {
-        self.current.address.len()
-    }
-
-    pub fn get_addres_ref(&self) -> &Vec<Address> {
-        &self.current.address
-    }
-}
-
-/// Height authority manage is an extensional data structure of authority list which means
+/// Authority manage is an extensional data structure of authority list which means
 /// `Vec<Node>`. It transforms the information in `Node` struct into a more suitable data structure
 /// according to its usage scene. The vote weight need look up by address frequently, therefore,
 /// address with vote weight saved in a `HashMap`.
-#[display(fmt = "{:?}", address)]
+#[display(fmt = "Authority List {:?}", address)]
 #[derive(Clone, Debug, Display, PartialEq, Eq)]
-struct HeightAuthorityManage {
+pub struct AuthorityManage {
     address:            Vec<Address>,
     propose_weights:    Vec<u64>,
     vote_weight_map:    HashMap<Address, u32>,
@@ -153,10 +23,10 @@ struct HeightAuthorityManage {
     vote_weight_sum:    u64,
 }
 
-impl HeightAuthorityManage {
+impl AuthorityManage {
     /// Create a new height authority manage.
-    fn new() -> Self {
-        HeightAuthorityManage {
+    pub fn new() -> Self {
+        AuthorityManage {
             address:            Vec::new(),
             propose_weights:    Vec::new(),
             vote_weight_map:    HashMap::new(),
@@ -166,7 +36,7 @@ impl HeightAuthorityManage {
     }
 
     /// Update the height authority manage by a new authority list.
-    fn update(&mut self, authority_list: &mut Vec<Node>) {
+    pub fn update(&mut self, authority_list: &mut Vec<Node>) {
         self.flush();
         authority_list.sort();
 
@@ -184,14 +54,14 @@ impl HeightAuthorityManage {
     }
 
     /// Get a vote weight of the node.
-    fn get_vote_weight(&self, addr: &Address) -> ConsensusResult<&u32> {
+    pub fn get_vote_weight(&self, addr: &Address) -> ConsensusResult<&u32> {
         self.vote_weight_map
             .get(addr)
             .ok_or_else(|| ConsensusError::InvalidAddress)
     }
 
     /// Get the proposer address by a given seed.
-    fn get_proposer(&self, height: u64, round: u64) -> ConsensusResult<Address> {
+    pub fn get_proposer(&self, height: u64, round: u64) -> ConsensusResult<Address> {
         let index = if cfg!(features = "random_leader") {
             get_random_proposer_index(
                 height + round,
@@ -214,7 +84,7 @@ impl HeightAuthorityManage {
     }
 
     /// Calculate whether the sum of vote weights from bitmap is above 2/3.
-    fn is_above_threshold(&self, bitmap: &[u8]) -> ConsensusResult<bool> {
+    pub fn is_above_threshold(&self, bitmap: &[u8]) -> ConsensusResult<bool> {
         let bitmap = BitVec::from_bytes(&bitmap);
         let mut acc = 0u64;
 
@@ -234,7 +104,7 @@ impl HeightAuthorityManage {
         Ok(acc * 3 > self.vote_weight_sum * 2)
     }
 
-    fn get_voters(&self, bitmap: &[u8]) -> ConsensusResult<Vec<Address>> {
+    pub fn get_voters(&self, bitmap: &[u8]) -> ConsensusResult<Vec<Address>> {
         let bitmap = BitVec::from_bytes(bitmap);
         let voters = bitmap
             .iter()
@@ -246,22 +116,31 @@ impl HeightAuthorityManage {
     }
 
     /// If the given address is in the current authority list.
-    fn contains(&self, address: &Address) -> bool {
+    pub fn contains(&self, address: &Address) -> bool {
         self.address.contains(address)
     }
 
     /// Get the sum of the vote weights in the current height.
-    fn get_vote_weight_sum(&self) -> u64 {
+    pub fn get_vote_weight_sum(&self) -> u64 {
         self.vote_weight_sum
     }
 
     /// Clear the HeightAuthorityManage, removing all values.
-    fn flush(&mut self) {
+    pub fn flush(&mut self) {
         self.address.clear();
         self.propose_weights.clear();
         self.vote_weight_map.clear();
         self.propose_weight_sum = 0;
         self.vote_weight_sum = 0;
+    }
+
+    /// Get the length of the current authority list.
+    pub fn len(&self) -> usize {
+        self.address.len()
+    }
+
+    pub fn get_addres_ref(&self) -> &Vec<Address> {
+        &self.address
     }
 }
 
@@ -294,7 +173,7 @@ mod test {
     use crate::error::ConsensusError;
     use crate::extract_voters;
     use crate::types::{Address, Node};
-    use crate::utils::auth_manage::{AuthorityManage, HeightAuthorityManage};
+    use crate::utils::auth_manage::AuthorityManage;
 
     fn gen_address() -> Address {
         Address::from((0..32).map(|_| random::<u8>()).collect::<Vec<_>>())
@@ -330,7 +209,7 @@ mod test {
     #[test]
     fn test_vote_weight() {
         let mut authority_list = gen_auth_list(0);
-        let mut authority_manage = HeightAuthorityManage::new();
+        let mut authority_manage = AuthorityManage::new();
         authority_manage.update(&mut authority_list);
 
         for node in authority_list.iter() {
@@ -357,40 +236,15 @@ mod test {
     #[test]
     fn test_update() {
         let mut authority_list = gen_auth_list(random::<u8>() as usize);
-        let mut authority_list_new = gen_auth_list(random::<u8>() as usize);
-        let mut authority_list_newer = gen_auth_list(random::<u8>() as usize);
         let mut auth_manage = AuthorityManage::new();
-        let mut e_auth_manage = HeightAuthorityManage::new();
-
-        auth_manage.update(&mut authority_list, false);
-        e_auth_manage.update(&mut authority_list);
-        assert_eq!(auth_manage, AuthorityManage {
-            current: e_auth_manage.clone(),
-            last:    None,
-        });
-
-        let mut e_auth_manage_old = e_auth_manage.clone();
-        auth_manage.update(&mut authority_list_new, true);
-        e_auth_manage.update(&mut authority_list_new);
-        assert_eq!(auth_manage, AuthorityManage {
-            current: e_auth_manage.clone(),
-            last:    Some(e_auth_manage_old),
-        });
-
-        e_auth_manage_old = e_auth_manage.clone();
-        auth_manage.update(&mut authority_list_newer, true);
-        e_auth_manage.update(&mut authority_list_newer);
-        assert_eq!(auth_manage, AuthorityManage {
-            current: e_auth_manage.clone(),
-            last:    Some(e_auth_manage_old),
-        });
-
-        auth_manage.update(&mut authority_list_new, false);
-        e_auth_manage.update(&mut authority_list_new);
-        assert_eq!(auth_manage, AuthorityManage {
-            current: e_auth_manage,
-            last:    None,
-        });
+        auth_manage.update(&mut authority_list);
+        assert_eq!(
+            auth_manage.address,
+            authority_list
+                .into_iter()
+                .map(|node| node.address)
+                .collect::<Vec<Address>>()
+        );
     }
 
     #[test]
@@ -403,30 +257,30 @@ mod test {
         ];
         authority_list.sort();
         let mut authority = AuthorityManage::new();
-        authority.update(&mut authority_list, false);
+        authority.update(&mut authority_list);
 
         for i in 0..4 {
             let bit_map = gen_bitmap(4, vec![i]);
-            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref(), true);
+            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref());
             assert_eq!(res.unwrap(), false);
         }
 
         let tmp = vec![vec![1, 2], vec![1, 3], vec![2, 3], vec![0, 1], vec![0, 2]];
         for i in tmp.into_iter() {
             let bit_map = gen_bitmap(4, i);
-            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref(), true);
+            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref());
             assert_eq!(res.unwrap(), false);
         }
 
         let tmp = vec![vec![0, 1, 2], vec![0, 1, 3], vec![1, 2, 3]];
         for i in tmp.into_iter() {
             let bit_map = gen_bitmap(4, i);
-            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref(), true);
+            let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref());
             assert_eq!(res.unwrap(), true);
         }
 
         let bit_map = gen_bitmap(4, vec![0, 1, 2, 3]);
-        let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref(), true);
+        let res = authority.is_above_threshold(Bytes::from(bit_map.to_bytes()).as_ref());
         assert_eq!(res.unwrap(), true);
     }
 
@@ -475,30 +329,30 @@ mod test {
         ];
         authority_list.sort();
         let mut authority = AuthorityManage::new();
-        authority.update(&mut authority_list, false);
+        authority.update(&mut authority_list);
 
         assert_eq!(
-            authority.get_proposer(1, 0, true).unwrap(),
+            authority.get_proposer(1, 0).unwrap(),
             authority_list[3].address
         );
         assert_eq!(
-            authority.get_proposer(1, 1, true).unwrap(),
+            authority.get_proposer(1, 1).unwrap(),
             authority_list[0].address
         );
         assert_eq!(
-            authority.get_proposer(2, 0, true).unwrap(),
+            authority.get_proposer(2, 0).unwrap(),
             authority_list[2].address
         );
         assert_eq!(
-            authority.get_proposer(2, 2, true).unwrap(),
+            authority.get_proposer(2, 2).unwrap(),
             authority_list[0].address
         );
         assert_eq!(
-            authority.get_proposer(3, 0, true).unwrap(),
+            authority.get_proposer(3, 0).unwrap(),
             authority_list[1].address
         );
         assert_eq!(
-            authority.get_proposer(3, 1, true).unwrap(),
+            authority.get_proposer(3, 1).unwrap(),
             authority_list[2].address
         );
     }
@@ -525,16 +379,16 @@ mod test {
     fn bench_update(b: &mut Bencher) {
         let mut auth_list = gen_auth_list(10);
         let mut authority = AuthorityManage::new();
-        b.iter(|| authority.update(&mut auth_list, true));
+        b.iter(|| authority.update(&mut auth_list));
     }
 
     #[bench]
     fn bench_cal_vote_weight(b: &mut Bencher) {
         let mut auth_list = gen_auth_list(10);
         let mut authority = AuthorityManage::new();
-        authority.update(&mut auth_list, false);
+        authority.update(&mut auth_list);
         let bitmap = BitVec::from_elem(10, true);
         let vote_bitmap = Bytes::from(bitmap.to_bytes());
-        b.iter(|| authority.is_above_threshold(&vote_bitmap, true));
+        b.iter(|| authority.is_above_threshold(&vote_bitmap));
     }
 }
